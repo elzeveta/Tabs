f3aae7519184d463de7341a5a0f58365b4b1f800
elzeveta <eliza.amanbaeva@gmail.com>
2021-07-01 23:32:47 -0700

    Routes

---
+++ tabs/src/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import History from "./components/History";
+import Goals from "./components/Goals";
+import Vision from "./components/Vision";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container">
+          <div className="main-image">
+            <img
+              src="https://vannilla-js-basic-project-11-tabs.netlify.app/hero-bcg.jpeg"
+              alt="images"
+            />
+          </div>
+          <div className="main">
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={History} />
+              <Route path="/vision" component={Vision} />
+              <Route path="/goals" component={Goals} />
+              <Route />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+}
